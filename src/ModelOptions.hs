b51e194106b8256801896b8aa9fe0eec6509bdc0
Sandeep.C.R <sandeepcr2@gmail.com>
2017-02-05 05:50:51 +0000

    Format options

--- src/ModelOptions.hs
+++ src/ModelOptions.hs
@@ -4,7 +4,27 @@ module ModelOptions where
 import Lib
 
 options = Options [
-   ("users", TableOptions "User" [("id", "UserId")] ["UserId"])
-  ,("products", TableOptions "Product" [("id", "ProductId"), ("user_id", "UserId")] ["ProductId"])
+   ("users", usersOptions)
+  ,("products", productsOptions)
   ]
+
+usersOptions = TableOptions {
+  modelName = "User",
+  overrideDefaultTypes = [
+    ("id", "UserId")
+  ],
+  generateInstancesFor = ["UserId"]
+}
+
+productsOptions = TableOptions {
+  modelName = "Product",
+  overrideDefaultTypes = [
+    ("id", "ProductId"),
+    ("user_id", "UserId")
+  ],
+  generateInstancesFor = ["ProductId"]
+}
+
+
+    
 
